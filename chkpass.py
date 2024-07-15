3783a5890aecace2066fbd36cff1d0b4796c834e
srn-source <nonesungsurapon@gmail.com>
2024-07-15 14:19:52 +0700

    add_admin8

--- chkpass.py
+++ chkpass.py
@@ -262,7 +262,7 @@ def app():
     # elif "ADMIN5" in st.session_state['userName'].upper(): 
     #     df_new = cursor.execute("SELECT TOP 1 * from View_vistec_check WHERE (url like '%www.longtunman.com%' or url like '%www.finnomena.com%' or url like '%khemmapat.org%' or url like '%www.lawsiam.com%' or url like '%srisunglaw.com%' or url like '%www.thanulegal.com%' or url like '%www.nstda.or.th%' or url like '%www.petcharavejhospital.com%') and review_status = 'PASS' and vistec_chk IS NULL and (article_id like '%6')")
     #     df_new = df_new.fetchall()
-    elif "ADMIN6" in st.session_state['userName'].upper(): 
+    elif "ADMIN8" in st.session_state['userName'].upper(): 
         df_new = cursor.execute("SELECT TOP 1 * from View_vistec_check WHERE type_domain = 'Medical' and  review_status = 'PASS' and vistec_chk IS NULL and (article_id like '%0' or article_id like '%8' )")
         df_new = df_new.fetchall()
     elif "ADMIN1" in st.session_state['userName'].upper(): 
